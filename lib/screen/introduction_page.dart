146e3d7e34e1daae8ffff9586ffc189dbe414e81
abircse45 <abiruodacse@gmail.com>
2022-06-08 16:48:03 +0600

    introduction_pge added

---
+++ lib/screen/introduction_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class IntroDuctionScreen extends StatelessWidget {
+  const IntroDuctionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Center(
+                child: Image.asset(
+              "assets/images/rebusel_image.jpeg",
+              fit: BoxFit.contain,
+              height: 50,
+            )),
+            SizedBox(
+              height: 60,
+            ),
+            Center(
+                child: Image.asset(
+              "assets/images/introduction.png",
+              fit: BoxFit.contain,
+              height: 170,
+            )),
+            SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: Text(
+                "Hello! Welcome",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 30, right: 30),
+              child: Center(
+                child: Text(
+                  "The First mate and his skipper too will ",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 40, right: 30),
+              child: Center(
+                child: Text(
+                  "do their very comfortable ",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: (){},
+              child: Center(
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 45,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: HexColor("#29AAE1"),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Text("Lets Go",style: TextStyle(fontSize: 15,color: Colors.white),),
+                ),
+              ),
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
